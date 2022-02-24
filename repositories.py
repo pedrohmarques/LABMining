a2852d5572287eaca9b8af9beb60b95eb9f103ab
Henrique <h.freire@live.com>
2022-02-24 13:17:56 -0300

    Add pagination

--- repositories.py
+++ repositories.py
@@ -4,10 +4,10 @@ class Repository:
     def __init__(self, token):
         self.token = token
     
-    def __get_query(self, primaryLanguage):
+    def __get_query(self, primaryLanguage, after = 'null'):
         query = f"""
         {{
-            search(query:"stars:>100 ", type:REPOSITORY, first:100){{
+            search(query:"stars:>100 ", type:REPOSITORY, first:100, after:{{after}}){{
                 nodes{{
                     ... on Repository {{
                         nameWithOwner
@@ -26,12 +26,18 @@ class Repository:
 
     def get_repositories(self, primaryLanguage, num_repositories):
         headers = headers = {'Authorization': f'Bearer {self.token}'}
-        query = self.__get_query(primaryLanguage)
-        result = requests.post("https://api.github.com/graphql", json={'query': query}, headers=headers)
-        print(result)
-        if result.status_code == 200:
-            data = result.json()['data']['search']
-            repositories = list(map(lambda x: x, data['nodes']))
-            print(f"\rRetrieve {len(repositories)} repositories")
-            return repositories
-            
+        after = 'null'
+        repositories = list
+        for i in range(10):
+            print("get repositories: ", i)
+            query = self.__get_query(primaryLanguage, after)
+            result = requests.post("https://api.github.com/graphql", json={'query': query}, headers=headers)
+
+            if result.status_code == 200:
+                data = result.json()['data']['search']
+                after = data['pageInfo']['endCursor']
+                repositories.__add__(list(map(lambda x: x, data['nodes'])))
+
+       print(f"\rRetrieve {len(repositories)} repositories")
+       return repositories
+
